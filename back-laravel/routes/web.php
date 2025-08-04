d03fbe195fcfb57e3586e0081f45188a7516dc7c
Fabricio Cervantes <fabriciocervantesmendoza@gmail.com>
2025-08-03 19:02:32 -0500

    Cambio de apis a api.php

--- back-laravel/routes/web.php
+++ back-laravel/routes/web.php
@@ -8,11 +8,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('players', [GolpeadoController::class,'index'])->name('players.index');
-Route::post('game', [GameController::class,'store'])->name('game.store');
-Route::put('game/cierre', [GameController::class,'update'])->name('game.cierre');
-Route::post('ronda', [RondaController::class,'store'])->name('ronda.store');
-Route::put('ronda/{ronda}/set-winner', [RondaController::class,'setWinner'])->name('ronda.setWinner');
-Route::post('ronda/{ronda}/add-players', [RondaController::class,'addPlayers'])->name('ronda.addPlayers');
-Route::delete('ronda/{ronda}/remove-players', [RondaController::class,'removePlayer'])->name('ronda.removePlayer');
